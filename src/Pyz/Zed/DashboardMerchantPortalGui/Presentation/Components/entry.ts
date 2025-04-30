aeb087da87ccb7cce1bb5993db317d2620ff21b9
Daniel Szubart <d.szubart@virtuacodelab.com>
2025-04-30 09:10:37 +0200

    marketplace angular component

--- src/Pyz/Zed/DashboardMerchantPortalGui/Presentation/Components/entry.ts
+++ src/Pyz/Zed/DashboardMerchantPortalGui/Presentation/Components/entry.ts
@@ -1,5 +1,6 @@
 import { registerNgModule } from '@mp/zed-ui';
-
+import { ComponentsModule as ComponentsModuleCore } from '@mp/dashboard-merchant-portal-gui';
 import { ComponentsModule } from './app/components.module';
 
+registerNgModule(ComponentsModuleCore);
 registerNgModule(ComponentsModule);
